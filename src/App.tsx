e42cb6f20ebee919967cd1c01a420e3f21a83427
Sergey <sergey.nuzhnyy@exactprosystems.com>
2022-10-03 12:13:33 +0000

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+  const [link, setLink] = useState();
+  const [linkObj, setLinkObj] = useState();
+
+  const getLinks = () => { 
+    fetch('./links.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setLinkObj(data?.links);
+      });
+  }
+
+  useEffect(() => getLinks(), []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="header__container">
+          <div className="app-link" onClick={() => setLink(linkObj?.["dashboard"])}>Dashboard</div>
+          <div className="app-link" onClick={() => setLink(linkObj?.["grafana"])}>Grafana</div>
+          <div className="app-link" onClick={() => setLink(linkObj?.["rabbitMq"])}>RabbitMQ</div>
+          <div className="app-link" onClick={() => setLink(linkObj?.["infraEditor"])}>Infra Editor</div>
+        </div>
       </header>
-    </div>
+      <iframe src={link} title="app window" className="iframe"/>
+    </>
   );
 }
 

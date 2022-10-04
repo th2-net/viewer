dd86653b9f1057af89dcf21f393c226ad2bfb5ac
Sergey <sergey.nuzhnyy@exactprosystems.com>
2022-10-04 10:02:54 +0000

    functional app + materialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, Tabs, Tab } from '@mui/material';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -5,7 +6,7 @@ import './App.scss';
 
 function App() {
 
-  const [link, setLink] = useState();
+  const [link, setLink] = useState<string | undefined>();
   const [linkObj, setLinkObj] = useState();
 
   const getLinks = () => { 
@@ -22,26 +23,36 @@ function App() {
       })
       .then(data => {
         console.log(data);
-        setLinkObj(data?.links);
+        setLinkObj(data);
       });
   }
 
   useEffect(() => getLinks(), []);
 
-  
+  const handleChange = (event: React.SyntheticEvent, newValue: string | undefined) => {
+    setLink(newValue);
+  };
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+      p: 2
+    };
+  }
 
   return (
-    <>
-      <header className="header">
-        <div className="header__container">
-          <div className="app-link" onClick={() => setLink(linkObj?.["dashboard"])}>Dashboard</div>
-          <div className="app-link" onClick={() => setLink(linkObj?.["grafana"])}>Grafana</div>
-          <div className="app-link" onClick={() => setLink(linkObj?.["rabbitMq"])}>RabbitMQ</div>
-          <div className="app-link" onClick={() => setLink(linkObj?.["infraEditor"])}>Infra Editor</div>
-        </div>
-      </header>
-      <iframe src={link} title="app window" className="iframe"/>
-    </>
+      <div className="container">
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Dashboard" {...a11yProps(0)} value={linkObj?.["dashboard"]}  />
+            <Tab label="Grafana" {...a11yProps(1)}  value={linkObj?.["grafana"]} />
+            <Tab label="RabbitMQ" {...a11yProps(2)} value={linkObj?.["rabbitMq"]} />
+            <Tab label="InfraEditor" {...a11yProps(3)} value={linkObj?.["infraEditor"]} />
+          </Tabs>
+        </Box>
+        <iframe src={link} title="app window" className="iframe" id="Iframe"/>
+      </div>
   );
 }
 
